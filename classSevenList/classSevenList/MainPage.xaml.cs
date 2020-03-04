c7fe96bc7a2e4f3130aa36ab05d985579b029b94
asnopek <asnopek486@gmail.com>
2020-03-03 19:15:08 -0600

    Add project files.

---
+++ classSevenList/classSevenList/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace classSevenList
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            (sender as ListView).SelectedItem = null;
+
+            if (args.SelectedItem != null)
+            {
+                PageDataViewModel pageData = args.SelectedItem as PageDataViewModel;
+                //Page page = (Page)Activator.CreateInstance(pageData.Title);
+                //await Navigation.PushAsync(page);
+                await DisplayAlert("Clicked!", pageData.Title + " " + pageData.Description, "OK");
+                }
+            }
+        }
+    }
---
+++ classSevenList/classSevenList/PageDataViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classSevenList
+{
+    public class PageDataViewModel
+    {
+        public PageDataViewModel(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public string Title { private set; get; }
+
+        public string Description { private set; get; }
+
+        static PageDataViewModel()
+        {
+            All = new List<PageDataViewModel>
+            {
+                new PageDataViewModel("Star Wars", "Fun Space Opera"),
+
+                new PageDataViewModel("Space Balls", "Another Fun Space Opera"),
+
+                new PageDataViewModel("The Revenge of Xaml", "For the Love of GOD"),
+
+            };
+        }
+
+        public static IList<PageDataViewModel> All { private set; get; }
+    }
+}
